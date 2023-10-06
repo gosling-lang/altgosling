2cf03ff29b2d84ff19dda4380c4164f9a6733e0e
thomcsmits <thomcsmits@outlook.com>
2023-10-06 17:14:18 -0400

    feat: get goslingcomponent in demo with bar chart

--- demo/Demo.tsx
+++ demo/Demo.tsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import gosling, { GoslingComponent } from 'gosling.js'
 import './Demo.css'
+import { bar } from './examples/bar';
 
 function Demo() {
+
+  const [goslingSpec, setGoslingSpec] = useState<gosling.GoslingSpec>();
+  const gosRef = useRef<gosling.GoslingRef>(null);
+  
+  useEffect(() => {
+    setGoslingSpec(bar)
+  }, []);
+
   return (
     <>
-      <p>Hello</p>
+      <div className='demo'>
+        bar
+        <GoslingComponent spec={goslingSpec} />
+      </div>
     </>
   )
 }
