be1180b710d31f25e597b76832746243835578d1
thomcsmits <thomcsmits@outlook.com>
2023-10-30 20:26:31 -0400

    fix: update import

--- src/alt-gosling-model/alt-data/alt-from-data.ts
+++ src/alt-gosling-model/alt-data/alt-from-data.ts
@@ -1,7 +1,8 @@
 //const _spec = models[0]?.spec(); _spec?.id
 import type { Datum } from 'gosling.js/dist/src/gosling-schema';
 import type { AltGoslingSpec, AltTrackDataFields, AltDataStatistics } from '../../schema/alt-gosling-schema';
-import { addTrackDataDescriptionsTrack, addTrackDescription } from '../alt-text/text-tree';
+import { addTrackDataDescriptionsTrack } from '../alt-text/text-data';
+import { addTrackDescription } from '../alt-text/text-global';
 
 export function altRetrieveDataStatistics(id: string, flatTileData: Datum[], dataFields: AltTrackDataFields): AltDataStatistics {
 
@@ -104,4 +105,10 @@ export function altUpdateSpecWithData(
     }
 
     return(altGoslingSpec);
+}
+
+export function altCreateDataPanel(
+    altDataStatistics: AltDataStatistics, 
+) {
+
 }
