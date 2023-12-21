7fdd9c760bf96102b90f475cc8bc3d1a63cfb5d2
thomcsmits <thomcsmits@outlook.com>
2023-12-21 04:34:10 -0500

    feat(demo): create radiogroup and render component based on selection

--- demo/DemoFull.tsx
+++ demo/DemoFull.tsx
@@ -1,159 +1,54 @@
-import React, { useEffect, useRef, useState } from 'react';
-import gosling, { GoslingComponent } from 'gosling.js';
+import React, { useState } from 'react';
+
 import { AltGoslingComponent } from '../src/AltGoslingComponent';
-import type { Datum } from 'gosling.js/dist/src/gosling-schema';
-// import type { AltGoslingSpec, PreviewAlt, AltTrack, AltDataStatistics } from '../src/schema/alt-gosling-schema';
-// import './Demo.css';
+
 import { bar } from './examples/bar';
 import { visualEncoding } from './examples/visualEncoding';
-// import { visualEncoding } from './examples/visualEncoding';
-// import Grid from '@mui/material/Grid';
-// import TextField from '@mui/material/TextField';
-// import Button from '@mui/material/Button';
 
-// import { getAlt, updateAlt } from '../src/alt-gosling-model';
-// import { renderAltTree, renderDataPanel } from '../src/render';
-
-
-// function Demo() {
-//     const gosRef = useRef<gosling.GoslingRef>(null);
-//     const goslingSpec = bar as gosling.GoslingSpec;
-  
-//     if (gosRef.current) {
-//       //rawData
-//       const currentRef = gosRef.current;
-//       currentRef.api.subscribe("rawData", (type, data) => {
-//         console.log("rawData", data);
-//       });
-//       // specProcessed
-//       currentRef.api.subscribe("specProcessed", (type, data) => {
-//         console.log("specProcessed", data);
-//       });
-//     }
-  
-//     return (
-//       <div>
-//         <GoslingComponent ref={gosRef} spec={goslingSpec} />
-//       </div>
-//     );
-//   }
+import Grid from '@mui/material/Grid';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 
 function Demo() {
-    const goslingSpec = bar as gosling.GoslingSpec;
-    const goslingSpec2 = visualEncoding as gosling.GoslingSpec;
+    const examples = {'bar': bar, 'visualEncoding': visualEncoding}
+    const [selectedExample, setSelectedExample] = useState<string>(Object.keys(examples)[0]);
+
+    const ExampleOptions = () => {
+        return (
+            <FormControl>
+                <FormLabel id='example-options'>Select example</FormLabel>
+                <RadioGroup
+                    row
+                    aria-labelledby="example-options-radio-button"
+                    defaultValue={Object.keys(examples)[0]}
+                    value={selectedExample}
+                    name="example-options-radio-button"
+                    onChange={(event) => setSelectedExample(event.target.value)}
+                >
+                    {Object.keys(examples).map(e => (
+                        <FormControlLabel value={e} control={<Radio />} label={e} key={'example-'+e}/>
+                    ))}
+                </RadioGroup>
+            </FormControl>
+        )
+    }
+
     return (
         <>
-            <p>hello</p>
-            <AltGoslingComponent spec={goslingSpec}/>
+            <Grid container spacing={1}>
+                <Grid item aria-label='example checkbox' xs={12}>
+                    <ExampleOptions/>
+                </Grid>
+                <Grid item aria-label='alt-gosling component' xs={12}>
+                    <AltGoslingComponent spec={examples[selectedExample]}/>
+                </Grid>
+            </Grid>         
        </>
    );
 }
 
-function Demo2() {
-    const goslingSpec = bar as gosling.GoslingSpec;
-    const goslingSpec2 = visualEncoding as gosling.GoslingSpec;
-    const gosRef = useRef<gosling.GoslingRef>(null);
-    
-  
-    interface AltGoslingCompProps {
-        spec?: gosling.GoslingSpec,
-        test?: boolean
-    }
-
-    
-
-    const AltGoslingComponent = (props: AltGoslingCompProps) => {
-        const gosRef = useRef<gosling.GoslingRef>(null);
-  
-         // subscribe to the gosRef specResolved and rawData apis
-        if (gosRef.current) {
-            const currentRef = gosRef.current;
-            // specProcessed
-            currentRef.api.subscribe("specProcessed", (_: string, data: {id: string, spec: gosling.GoslingSpec}) => {
-                console.log("specProcessed", data);
-
-               
-            });
-            //rawData
-            currentRef.api.subscribe("rawData", (_: string, data: {id: string, data: Datum[]}) => {
-                console.log("rawData", data);
-
-            });
-        }
-
-        return(
-            <>
-                <GoslingComponent ref={gosRef} {...props}/>
-            </>
-        );
-    };
-
-
-
-    return (
-        <>
-            <p>hello</p>
-            <AltGoslingComponent spec={goslingSpec} test={true}/>
-        </>
-    );
-}
-
 export default Demo;
-
-
-
-
-    // const AltGoslingComponent = (props: AltGoslingCompProps) => {
-    //     const gosRef = useRef<gosling.GoslingRef>(null);
-  
-    //      // subscribe to the gosRef specResolved and rawData apis
-    //     if (gosRef.current) {
-    //         const currentRef = gosRef.current;
-    //         // specProcessed
-    //         currentRef.api.subscribe("specProcessed", (_: string, data: {id: string, spec: GoslingSpec}) => {
-    //             console.log("specProcessed", data);
-    //             // get AltGoslingSpec
-    //             const altSpec = getAlt(data.spec);
-
-    //             // update current alt
-    //             updateDisplay(altSpec);
-    //             console.log(previewAlt.current);
-
-    //         });
-    //         //rawData
-    //         currentRef.api.subscribe("rawData", (_: string, data: {id: string, data: Datum[]}) => {
-    //             console.log("rawData", data);
-
-    //             // get latest AltGoslingSpec
-    //             const updatedAlt = updateAlt(previewAlt.current[selectedPreviewAlt].data, data.id, data.data)
-    //             updateDisplay(updatedAlt);
-                
-    //             setPreviewDataPanel([updatedAlt.tracks[0], updatedAlt.tracks[0].data.details.dataStatistics]);
-    //         });
-    //     }
-
-
-    //     export const AltGoslingComponent = forwardRef<GoslingRef>((props, gosRef) => {
-
-    //         useEffect(() => {
-    //             const token = gosRef.current.api.subscribe('rawData', (_: string, data: {id: string, data: Datum[]}) => {
-    //                     console.log('Updated data was seen for', data.id);
-    //               });
-    //             return () => {
-    //               gosRef.current?.api.unsubscribe(token);
-    //             }
-    //           }, [gosRef.current]);
-            
-    //         return (
-    //             <></>
-    //         )
-
-        
-    //     return(
-    //         <>
-    //             <GoslingComponent ref={gosRef} {...props}/>
-    //         </>
-    //     )
-
-    // }
