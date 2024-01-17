1393ad554f26825bfa5accb9f03824cad050614b
thomcsmits <thomcsmits@outlook.com>
2024-01-17 14:41:26 -0500

    docs: add jdoc for util functions, remove redundant booleanToString function

--- src/render/util.ts
+++ src/render/util.ts
@@ -1,15 +1,13 @@
+/**
+ * Function for concatination an array into a natural language sentence
+ * @param arr array with elements of string or number
+ * @returns string with elements concatenated with commas and 'and'
+ * E.g., if arr is ['first', 'second', 'third'], this will return 'first, second and third'
+ */
 export function arrayToString(arr: string[] | number[]): string {
     if (arr.length === 1) {
         return arr[0] as string;
     } else {
         return arr.slice(0, -1).join(', ') + ' and ' + arr.slice(-1);
-    }
-}
-
-export function booleanToString(b: boolean): string {
-    if (b) {
-        return 'true';
-    } else {
-        return 'false';
     }
 }
