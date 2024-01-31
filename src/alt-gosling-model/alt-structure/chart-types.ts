d0c28641eaaaba7dde4f6e79298cc91fd775a26f
thomcsmits <thomcsmits@outlook.com>
2024-01-31 18:21:23 -0500

    feat: add circularity in chartType

--- src/alt-gosling-model/alt-structure/chart-types.ts
+++ src/alt-gosling-model/alt-structure/chart-types.ts
@@ -14,36 +14,42 @@ export function determineSpecialCases(altTrack: AltTrackSingle | AltTrackOverlai
     const _valueEncodings = altTrack.appearance.details.encodings.encodingValue.map(o => o.name);
     const _allEncodings = [..._genomicEncodings, ..._quantitativeEncodings, ..._nominalEncodings, ..._valueEncodings];
 
-    if (_mark === 'point' && _quantitativeEncodings.includes('x') && _quantitativeEncodings.includes('y')) {
-        return 'scatter plot';
-    }
-    if (_mark === 'line' && _genomicEncodings.includes('x') && _quantitativeEncodings.includes('y')) {
-        return 'line chart';
-    }
-    if (_mark === 'line' && _quantitativeEncodings.includes('x') && _genomicEncodings.includes('y')) {
-        return 'line chart';
-    }
-    if (_mark === 'bar' && _genomicEncodings.includes('x') && _quantitativeEncodings.includes('y')) {
-        return 'bar chart';
-    }
-    if (_mark === 'rect' && _genomicEncodings.includes('x') && _genomicEncodings.includes('xe') && _quantitativeEncodings.includes('color')) {
-        return 'heat map';
-    }
-    if (_mark === 'rect' && _genomicEncodings.includes('x') && _genomicEncodings.includes('xe') && _nominalEncodings.includes('color')) {
-        return 'ideogram';
-    }
-    if (_mark === 'rule' && _allEncodings.includes('x') && _allEncodings.includes('y')) {
-        return 'chart with lines';
-    }
-    if (_mark === 'rule' && _allEncodings.includes('x')) {
-        return 'chart with vertical lines';
-    }
-    if (_mark === 'rule' && _allEncodings.includes('y')) {
-        return 'chart with horizontal lines';
+    const _layout = altTrack.appearance.details.layout;
+    if (_layout !== 'circular') {
+        if (_mark === 'point' && _quantitativeEncodings.includes('x') && _quantitativeEncodings.includes('y')) {
+            return 'scatter plot';
+        }
+        if (_mark === 'line' && _genomicEncodings.includes('x') && _quantitativeEncodings.includes('y')) {
+            return 'line chart';
+        }
+        if (_mark === 'line' && _quantitativeEncodings.includes('x') && _genomicEncodings.includes('y')) {
+            return 'line chart';
+        }
+        if (_mark === 'bar' && _genomicEncodings.includes('x') && _quantitativeEncodings.includes('y')) {
+            return 'bar chart';
+        }
+        if (_mark === 'rect' && _genomicEncodings.includes('x') && _genomicEncodings.includes('xe') && _quantitativeEncodings.includes('color')) {
+            return 'heat map';
+        }
+        if (_mark === 'rect' && _genomicEncodings.includes('x') && _genomicEncodings.includes('xe') && _nominalEncodings.includes('color')) {
+            return 'ideogram';
+        }
+        if (_mark === 'rule' && _allEncodings.includes('x') && _allEncodings.includes('y')) {
+            return 'chart with lines';
+        }
+        if (_mark === 'rule' && _allEncodings.includes('x')) {
+            return 'chart with vertical lines';
+        }
+        if (_mark === 'rule' && _allEncodings.includes('y')) {
+            return 'chart with horizontal lines';
+        }
+        if (markToText.get(_mark)) {
+            return `chart with ${markToText.get(_mark)}`;
+        } 
     }
     if (markToText.get(_mark)) {
-        return `chart with ${markToText.get(_mark)}`;
-    } 
+        return `circular chart with ${markToText.get(_mark)}`;
+    }
     
     return `unknown chart`;
 }
