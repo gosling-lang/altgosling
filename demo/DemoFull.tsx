aba7e4db984b8312c4c74a1b5723e5a589f9e3ce
thomcsmits <thomcsmits@outlook.com>
2024-06-10 12:49:01 -0400

    fix: order and rename demo examples

--- demo/DemoFull.tsx
+++ demo/DemoFull.tsx
@@ -28,15 +28,15 @@ import MenuItem from '@mui/material/MenuItem';
 
 function Demo() {
     const examples = {'Bar chart': barChart,
-                      'Ideogram': ideogramWithArea,
-                      'Linked views': brush,
-                      'Tonsil ChIP-seq comparison': compareTwoSamples,
-                      'Bar chart with lines': ruleMark,
-                      'Double marks': doubleMarks,
-                      'Gene annotations': geneAnnotation,
-                      'Circular halves': circularHalves,
                       'Heatmap': heatmap,
-                      'Matrix': matrix
+                      'Matrix': matrix,
+                      'Comparison of four samples': doubleMarks,
+                      'Annotated chart': ruleMark,
+                      'Comparing two samples': compareTwoSamples,
+                      'Linked views': brush,
+                      'Circular halves': circularHalves,
+                      'Gene annotations': geneAnnotation,
+                      'Ideogram expression': ideogramWithArea,
                     };
     const [selectedExample, setSelectedExample] = useState<string>(Object.keys(examples)[0]);
 
