f5b61a974b0d410de5099ecf038123aa54c44b85
thomcsmits <thomcsmits@outlook.com>
2024-10-23 19:24:14 -0400

    fix links docs

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -19,12 +19,12 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className={clsx("button button--secondary button--lg", styles.button)}
-            to="/docs/getting-started">
+            to="/docs/docs/getting-started">
             Get Started
           </Link>
           <Link
             className={clsx("button button--secondary button--lg", styles.button)}
-            to="/demo">
+            to="https://gosling-lang.github.io/altgosling/">
             Try a Demo
           </Link>
           <Link
