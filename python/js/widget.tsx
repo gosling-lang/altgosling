b998502c6fc77dc4fe94dab0b5e272101957830f
thomcsmits <thomcsmits@outlook.com>
2025-02-20 17:14:36 -0500

    update widget for example barchart

--- python/js/widget.tsx
+++ python/js/widget.tsx
@@ -1,14 +1,44 @@
 import * as React from "react";
 import { createRender, useModelState } from "@anywidget/react";
+import * as altgosling from "altgosling";
 import "./widget.css";
 
+
+export const barChart = {
+	"title": "Tonsil ChIP-seq in Th1 cells",
+	"tracks": [
+	  {
+		"layout": "linear",
+		"width": 800,
+		"height": 180,
+		"data": {
+		  "url": "https://resgen.io/api/v1/tileset_info/?d=UvVPeLHuRDiYA3qwFlm7xQ",
+		  "type": "multivec",
+		  "row": "sample",
+		  "column": "position",
+		  "value": "peak",
+		  "categories": ["sample 1"],
+		  "binSize": 5
+		},
+		"mark": "bar",
+		"x": {"field": "start", "type": "genomic", "axis": "bottom"},
+		"xe": {"field": "end", "type": "genomic"},
+		"y": {"field": "peak", "type": "quantitative", "axis": "right"},
+		"size": {"value": 5}
+	  }
+	]
+  }
+
+
+
 const render = createRender(() => {
-	const [value, setValue] = useModelState<number>("value");
+
+	console.log("component", altgosling.AltGoslingComponent);
 	return (
 		<div className="altgosling">
-			<button onClick={() => setValue(value + 1)}>
-				count is {value}
-			</button>
+			{/* <h1>Hello</h1> */}
+			{/* <altgosling.AltGoslingComponent /> */}
+			<altgosling.AltGoslingComponent spec={barChart} />
 		</div>
 	);
 });
